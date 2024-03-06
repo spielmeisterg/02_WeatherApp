6521ecdf7e4187130acf6a038c0b0312956ae992
spielmeisterg <ianspeelman@gmail.com>
2024-03-06 19:49:52 +0100

    starting js

---
+++ script/script.js
@@ -0,0 +1,8 @@
+"use strict"
+const current = {
+    currentTemp: document.getElementById("current_temperature"),
+    currentHumidity: document.getElementById("humidity"),
+    currentWindSpeed: document.getElementById("current_windSpeed"),
+    currentWindDirection: document.getElementById("current_windDirection"),
+}
+console.log(current);
